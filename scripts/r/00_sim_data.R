92851e3755f34a2514b9e094ef627d950d24c405
Timo Roettger <timoroettger@Timos-MacBook-Pro-2.local>
2020-11-17 16:19:08 +0100

    changed sim to include dummy columns for later distance columns

--- scripts/r/00_sim_data.R
+++ scripts/r/00_sim_data.R
@@ -12,12 +12,6 @@
 ## libraries--------------------------------------------------------------------
 source(here::here("scripts", "r", "00_libs.R"))
 
-
-
-# get path
-current_path = rstudioapi::getActiveDocumentContext()$path 
-setwd(dirname(current_path))
-
 ## set parameters---------------------------------------------------------------
 
 # number of observations
@@ -29,11 +23,11 @@ meta_effect = 1
 # probability of peer verdict
 prob_peer_verdict = c(0.2, 0.6, 0.15, 0.05)
 
-# lambda for fixed effect
-fixed_lambda = 3
+# probability for fixed effect
+fixed_prob = 0.3
 
-# lambda for random effect
-random_lambda = 3
+# probability for random effect
+random_prob = 0.5
 
 # probability of posthoc changes
 post_prob = 0.2
@@ -62,10 +56,20 @@ dv_c = c()
 se_c = c()
 peer_rating_c = c()
 peer_verdict_c = c()
-fixed_c = c()
-random_c = c()
 posthoc_c = c()
 nmodels_c = c()
+
+# create logical vectors for different fixed & random parameters
+fixed_1_c = c()
+fixed_2_c = c()
+fixed_3_c = c()
+fixed_4_c = c()
+fixed_5_c = c()
+random_1_c = c()
+random_2_c = c()
+random_3_c = c()
+random_4_c = c()
+random_5_c = c()
 
 
 for (i in 1:nteams) { 
@@ -89,13 +93,27 @@ for (i in 1:nteams) {
   
   # generate predictors
   
-  # number of fixed effect parameters (poisson distributed)
-  fixed_c <- c(fixed_c,
-               rpois(n = nobs, lambda = fixed_lambda))
-
-  # number of random effect parameters (poisson distributed)
-  random_c <- c(random_c,
-                rpois(n = nobs, lambda = random_lambda))
+  # whether fixed and random parameters are used or not
+  fixed_1_c <- c(fixed_1_c, 
+                 rbinom(n = nobs, size = 1, prob = fixed_prob))
+  fixed_2_c <- c(fixed_2_c, 
+                 rbinom(n = nobs, size = 1, prob = fixed_prob))
+  fixed_3_c <- c(fixed_3_c, 
+                 rbinom(n = nobs, size = 1, prob = fixed_prob))
+  fixed_4_c <- c(fixed_4_c, 
+                 rbinom(n = nobs, size = 1, prob = fixed_prob))
+  fixed_5_c <- c(fixed_5_c, 
+                 rbinom(n = nobs, size = 1, prob = fixed_prob))
+  random_1_c <- c(random_1_c, 
+                 rbinom(n = nobs, size = 1, prob = random_prob))
+  random_2_c <- c(random_2_c, 
+                 rbinom(n = nobs, size = 1, prob = random_prob))
+  random_3_c <- c(random_3_c, 
+                 rbinom(n = nobs, size = 1, prob = random_prob))
+  random_4_c <- c(random_4_c, 
+                 rbinom(n = nobs, size = 1, prob = random_prob))
+  random_5_c <- c(random_5_c, 
+                 rbinom(n = nobs, size = 1, prob = random_prob))
 
   # post hoc changes to analysis (yes vs. no)
   posthoc_c <- c(posthoc_c,
@@ -112,11 +130,26 @@ for (i in 1:nteams) {
 df <- data.frame(team = team_c,
                  es = dv_c,
                  se = se_c,
-                 fixed = fixed_c,
-                 random = random_c,
+                 fixed_1 = fixed_1_c,
+                 fixed_2 = fixed_2_c,
+                 fixed_3 = fixed_3_c,
+                 fixed_4 = fixed_4_c,
+                 fixed_5 = fixed_5_c,
+                 random_1 = random_1_c,
+                 random_2 = random_2_c,
+                 random_3 = random_3_c,
+                 random_4 = random_4_c,
+                 random_5 = random_5_c,
                  posthoc = posthoc_c,
                  nmodels = nmodels_c
 )
+
+# sum up fixed and random effects
+df <- df %>% 
+  mutate(fixed_num = fixed_1 + fixed_2 + fixed_3 + fixed_4 + fixed_5,
+         random_num = random_1 + random_2 + random_3 + random_4 + random_5
+  )
+
 
 ## add reviewer infos-----------------------------------------------------------
 
@@ -135,7 +168,7 @@ split <- data.frame(split_list) %>%
                values_to = "team") %>% 
   group_by(reviewer) %>% 
   # create random number that represents reviewer bias
-  mutate(random_factor = rnorm(1, 0, rand_revs))
+  mutate(reviewer_noise = rnorm(1, 0, rand_revs))
 
 # combine data frames
 df2 <- full_join(df, split)
@@ -146,7 +179,7 @@ for (j in 1:nrow(df2)) {
   print(paste0("random sample # ", j))
   # peer review ratings (0-100)
   df2$peer_rating[j] <- rtruncnorm(nobs, a = 0, b = 100, 
-                                   mean = 50 *df2$random_factor[j], 
+                                   mean = 50 *df2$reviewer_noise[j], 
                                    sd = 15)
   # peer verdict (ordinal)
   df2$peer_verdict[j] <- sample(x = c("reject", "major", "minor", "accept"), 
