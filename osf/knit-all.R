4fdcf42596506e62212134b35a3917a22d298924
stefanocoretta <stefano.coretta@gmail.com>
2021-01-05 10:25:33 +0100

    update renv packages; delete unused R file

--- osf/knit-all.R
+++
@@ -1,3 +0,0 @@
-files <- list.files("./osf/comps", pattern = "[.]Rmd$", recursive = T, full.names = T)
-for (f in files) rmarkdown::render(f, knit_root_dir = here::here(), output_format = "github_document", envir = new.env())
-
